4da45d948f67ff319063365e0ba813b153a4cf36
Mike Tung <miketung2013@gmail.com>
2019-11-10 09:19:49 -0500

    added team endpoint and infrastructure

--- src/main/java/com/seekheart/superpalapi/repository/TeamRepository.java
+++ src/main/java/com/seekheart/superpalapi/repository/TeamRepository.java
@@ -1,6 +1,7 @@
 package com.seekheart.superpalapi.repository;
 
 import com.seekheart.superpalapi.model.domain.Team;
+import java.util.Optional;
 import java.util.UUID;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -8,4 +9,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface TeamRepository extends CrudRepository<Team, UUID> {
 
+  Optional<Team> findByName(String name);
 }
+
